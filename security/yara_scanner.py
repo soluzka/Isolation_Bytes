f189efa5c7c8f0fce4ab38c4d53a13f67f5d5161
soluzka is a boss <bpiersing@outlook.com>
2026-09-16 05:35:11 -0400

    Restore YARA scanner source

--- security/yara_scanner.py
+++ security/yara_scanner.py
@@ -1,1 +1,9 @@
-PLACEHOLDER
+import yara as yara_module
+import os
+import logging
+import sys
+import time
+import functools
+import warnings
+import re
+from yara import Error as YaraError, TimeoutError as YaraTimeoutError
